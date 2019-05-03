b530524b9cb369c93ab10d01cce3ba2fd79b37a1
Juan Diego Villalobos Quirós <juandiegovillalobosquiros@gmail.com>
2019-05-03 17:36:38 +0200

    JuegoDelaVidaV2 updated.

--- clases/Main.c
+++ clases/Main.c
@@ -3,14 +3,18 @@
 //
 
 #include <stdlib.h>
+#include <printf.h>
 
 void abrirPrimeraClase();
 
-/*
-void main(){
+
+void MainFunction(){
+    printf("¿Qué quieres realizar?\n");
+    printf("1 - Ejecutar código fuente de las clases.\n");
+    printf("2 - Ejecutar código del Juego de la Vida (Game Of Life).");
+
     abrirPrimeraClase();
 }
-*/
 
 void abrirPrimeraClase(){
     system("open https://www.youtube.com/watch?v=C0UiG1uYgYQ&feature=youtu.be");
